acfb16b4ad62bd8841e8a69294fe8d1fa905541b
hshk1004 <hshk_1004@naver.com>
2024-08-12 15:27:32 +0900

    Created index.js
    made index.js and add function names for simple math and data form transformation. with a little notion

---
+++ index.js
@@ -0,0 +1,149 @@
+// this codes will writen in js using node.js and maybe express.js and i hope i will make same copy for typescript
+
+// i hpoe this repo to be a lib for various js project
+
+// on this page there will be index of functions and function contents explanations'
+
+// before function is complete whole funtion will be here
+
+// function's add and delete must be written on commit's comment
+
+// optimization will done after most of the function is written
+
+function main(){
+    return 0;
+}
+
+function Math(){
+    function add(){
+
+    }
+    function minus(){
+
+    }
+    function multiply(){
+
+    }
+    function divide(){
+
+    }
+    function remain(){
+
+    }
+
+}
+
+
+// this function is for transform one data to another form of data
+// such as xml to json or yaml to toml
+// kind of forms are *string, json, csv, xml, yaml, toml*
+function Transform(){
+    
+    function dataTostring(){
+        function jsonTostring(){
+
+        }
+        function xmlTostring(){
+
+        }
+        function csvTostring(){
+
+        }
+        function yamlTostring(){
+
+        }
+        function tomlTostring(){
+
+        }
+    }
+
+    function dataTojson(){
+        function stringTojson(){
+
+        }
+        function xmlTojson(){
+
+        }
+        function csvTojson(){
+
+        }
+        function yamlTojson(){
+
+        }
+        function tomlTojson(){
+
+        }
+    }
+
+    function dataToxml(){
+        function jsonToxml(){
+
+        }
+        function stringToxml(){
+
+        }
+        function csvToxml(){
+
+        }
+        function yamlToxml(){
+
+        }
+        function tomlToxml(){
+
+        }
+    }
+
+    function dataTocsv(){
+        function jsonTocsv(){
+
+        }
+        function xmlTocsv(){
+
+        }
+        function stringTocsv(){
+
+        }
+        function yamlTocsv(){
+
+        }
+        function tomlTocsv(){
+
+        }
+    }
+        
+    function dataToyaml(){
+        function jsonToyaml(){
+
+        }
+        function xmlToyaml(){
+
+        }
+        function csvToyaml(){
+
+        }
+        function stringToyaml(){
+
+        }
+        function tomlToyaml(){
+
+        }
+    }
+
+    function dataTotoml(){
+        function jsonTotoml(){
+
+        }
+        function xmlTotoml(){
+
+        }
+        function csvTotoml(){
+
+        }
+        function yamlTotoml(){
+
+        }
+        function stringTotoml(){
+
+        }
+    }
+}
